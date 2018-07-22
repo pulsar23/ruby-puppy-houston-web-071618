ac5277c74d98203fccf2446519309a2de156067f
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-22 15:19:22 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each do |pups|
+      puts "#{pups.name}"
+    end
+  end
+
+end #class Dog
